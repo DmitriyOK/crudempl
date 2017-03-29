94683dda26c09e5173e2f133cac0e795dfbdc46d
dmitriy <dima5552004@yandex.ru>
2017-03-30 00:50:58 +0300

    фикс ошибок. Проект работает

--- src/main/java/ru/petshop/company/controller/MainController.java
+++ src/main/java/ru/petshop/company/controller/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import ru.petshop.company.dao.DepartmentDao;
+import ru.petshop.company.dao.EmployerDao;
 import ru.petshop.company.service.DepartmentService;
 import ru.petshop.company.service.EmployerService;
 
@@ -17,9 +19,12 @@ public class MainController {
     @Autowired
     DepartmentService departmentService;
 
+    @Autowired
+    DepartmentDao departmentDao;
+
     @ResponseBody
     @RequestMapping(value = "index")
     public String main(){
-        return employerService.findOne(1).toString();
+        return departmentDao.findOne(1).toString();
     }
 }
